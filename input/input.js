a649481d348436773fb2d40ef27b93052879de6e
Morgan LaCouture <morganlea18@gmail.com>
2017-08-23 11:40:16 -0700

    played around with css

--- input/input.js
+++ input/input.js
@@ -259,7 +259,7 @@ var database = {
 
 function greetUser() {
     var elGreeting = document.getElementById('greeting');
-    elGreeting.innerText = 'Hi ' + user.name + '......................';
+    elGreeting.innerText = 'Hi ' + user.name + '!  ' + '  Pick your preferences';
 }
 
 function onRunInput() {
